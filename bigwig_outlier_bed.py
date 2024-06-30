b8dc3accc560340de300064032755325eacf1ce1
fubar2 <ross.lazarus@gmail.com>
2024-06-30 10:03:25 +1000

    Remove scipy signal find peaks code - not useful at present... Cleanup code. Add a printed table of contig results for each bigwig - make that an optional output? Ready for a tool..

--- bigwig_outlier_bed.py
+++ bigwig_outlier_bed.py
@@ -3,106 +3,93 @@ import numpy as np
 import pybigtools
 import sys
 
-from scipy.signal import find_peaks
-from scipy.signal import find_peaks_cwt
 
 """
 find bigwig regions above and below a chosen percentile point.
+Multiple bigwigs **with the same reference** can be combined - bed segments will be named appropriately
+Combining multiple references works but is silly because only display will rely on one reference so others will not be shown...
+
 0.01 works well for very large sequences with a minimum bed window size of 10 
 Tricksy numpy method from http://gregoryzynda.com/python/numpy/contiguous/interval/2019/11/29/contiguous-regions.html
 takes about 95 seconds for a 17MB test wiggle
+
+JBrowse2 bed normally displays ignore the score, so probably need options for separate low/high bed file outputs.
+
+Update june 30 2024: wrote a 'no-build' plugin for beds to display red/blue if >0/<0 so those are used for scores
+Bed interval naming must be short for JB2 but needs input bigwig name and (lo or hi).
+
 """
 
 class findOut():
 
-    def __init__(self, bwname="test.bw", qfrac=0.01, bedwin=10, bedout="test.bw.bed"):
+    def __init__(self, bwname=["test.bw",], bwlabels=["test",], qlo=0.01, qhi=0.99, bedwin=10, bedout="test.bw.bed"):
         self.bedwin = bedwin
-        self.qfrac = qfrac
-        self.bwf = pybigtools.open(bwname)
-        self.chrlist = self.bwf.chroms()
-        self.bedf = open(bedout, "w")
-        self.makeBed()
-        self.bedf.close()
+        self.qhi = qhi
+        self.qlo = qlo
+        self.makeBed(bwname[0], bwlabels[0], bedout)
 
-    def makeBedPeaks(self):
-        # use scipy find_peaks - not looking for signal "peaks" but interesting...
-        bed = []
-        chrs = list(self.chrlist.keys())
-        chrs.sort()
-        print(chrs)
-
-        for chr in chrs:
-            chrlen = self.chrlist[chr]
-            print(chr, chrlen)
-            nb = int(chrlen/self.bedwin) - 1
-            bw = self.bwf.values(chr)
-            bw = bw[~np.isnan(bw)] # some have NaN if parts of a contig not covered
-            bwtop = np.quantile(bw,0.99)
-            bwbot = np.quantile(bw,0.01)
-            bwmean = np.mean(bw)
-            bwstd = np.std(bw)
-            bwmax = np.max(bw)
-            bwhi, bwhiprop = find_peaks(bw, height=bwtop, width=self.bedwin, distance=20, prominence=(bwtop-bwbot)/10, wlen=100*self.bedwin) 
-            bwlo, bwloprop = find_peaks(-bw, height= -1*bwmax, width=self.bedwin, prominence=(bwtop-bwbot)/10, distance=20, wlen=100*self.bedwin) 
-            print('n=%d, got %d hi, %d low, topcut=%f, botcut=%f, mean=%f, std=%f, max=%f' % (np.size(bw), len(bwhi), len(bwlo), bwtop, bwbot, bwmean, bwstd, bwmax))
-            for i, peak in enumerate(bwhi):
-                bed.append((chr, peak, peak+bwhiprop["widths"][i],'%s_%d' % (chr,i), 1))
-            for i, peak in enumerate(bwlo):
-                bed.append((chr, peak, peak+bwloprop["widths"][i],'%s_%d' % (chr,i),-1))
-        bed.sort()
-        beds = ['%s\t%d\t%d\t%s\t%d' % x for x in bed]
-        self.bedf.write('\n'.join(beds))
-        self.bedf.write('\n')
+    def processVals(self, bw, isTop):
+        # http://gregoryzynda.com/python/numpy/contiguous/interval/2019/11/29/contiguous-regions.html
+        if isTop:
+            bwex = np.r_[False, bw >= self.bwtop, False] # extend with 0s
+        else:
+            bwex = np.r_[False, bw <= self.bwbot, False]
+        bwexd = np.diff(bwex)
+        bwexdnz = bwexd.nonzero()[0]
+        bwregions = np.reshape(bwexdnz, (-1,2))
+        return bwregions
         
-    def makeBed(self):
+    def makeBed(self, bwnames, bwlabels, bedout):
+        bedf = open(bedout, "w")
         bed = []
-        chrs = list(self.chrlist.keys())
-        chrs.sort()
-        restab = ["contig\tn\tmean\tstd\tmin\tmax\t"]
-        for chr in chrs:
-            chrlen = self.chrlist[chr]
-            nb = int(chrlen/self.bedwin) - 1
-            bw = self.bwf.values(chr)
-            bw = bw[~np.isnan(bw)] # some have NaN if parts of a contig not covered
-            bwtop = np.quantile(bw, 1.0-self.qfrac)
-            bwbot = np.quantile(bw, self.qfrac)
-            # http://gregoryzynda.com/python/numpy/contiguous/interval/2019/11/29/contiguous-regions.html
-            bwhiex = np.r_[False, bw >= bwtop, False] # extend with 0s
-            bwloex = np.r_[False, bw <= bwbot, False]
-            bwhid = np.diff(bwhiex)
-            bwlod = np.diff(bwloex)
-            bwhii = bwhid.nonzero()[0]
-            bwloi = bwlod.nonzero()[0]
-            bwhi = np.reshape(bwhii, (-1,2))
-            bwlo = np.reshape(bwloi, (-1,2))  
-            for i, seg in enumerate(bwhi):
-                if seg[1] - seg[0] >= self.bedwin:
-                    bed.append((chr, seg[0], seg[1], '%s_%d' % (chr,i), bwtop))
-            for i, seg in enumerate(bwlo):
-                if seg[1] - seg[0] >= self.bedwin:
-                    bed.append((chr, seg[0], seg[1], '%s_%d' % (chr,i), bwbot))
-            bwmean = np.mean(bw)
-            bwstd = np.std(bw)
-            bwmax = np.max(bw)
-            nrow = np.size(bw)
-            bwmin = np.min(bw)
-            restab.append('%s\t%d\t%f\t%f\t%f\t%f' % (chr,nrow,bwmean,bwstd,bwmin,bwmax))
+        bwlabels = bwlabels.split(',')
+        bwnames = bwnames.split(',')
+        print('bwnames=', bwnames, "bwlabs=", bwlabels)
+        for i, bwname in enumerate(bwnames):
+            bwlabel = bwlabels[i].replace(" ",'')
+            bwf = pybigtools.open(bwname)
+            chrlist = bwf.chroms()
+            chrs = list(chrlist.keys())
+            chrs.sort()
+            restab = ["contig\tn\tmean\tstd\tmin\tmax\tqtop\tqbot"]
+            for chr in chrs:
+                bw = bwf.values(chr)
+                bw = bw[~np.isnan(bw)] # some have NaN if parts of a contig not covered
+                self.bwtop = np.quantile(bw, self.qhi)
+                self.bwbot = np.quantile(bw, self.qlo)
+                if self.qhi is not None:
+                    bwhi = self.processVals(bw, isTop=True)
+                    for i, seg in enumerate(bwhi):
+                        if seg[1] - seg[0] >= self.bedwin:
+                            bed.append((chr, seg[0], seg[1], '%s_hi' % (bwlabel), 1))
+                if self.qlo is not None:
+                    bwlo = self.processVals(bw, isTop=False)            
+                    for i, seg in enumerate(bwlo):
+                        if seg[1] - seg[0] >= self.bedwin:
+                            bed.append((chr, seg[0], seg[1], '%s_lo' % (bwlabel), -1))
+                bwmean = np.mean(bw)
+                bwstd = np.std(bw)
+                bwmax = np.max(bw)
+                nrow = np.size(bw)
+                bwmin = np.min(bw)
+                restab.append('%s\t%d\t%f\t%f\t%f\t%f\t%f\t%f' % (chr,nrow,bwmean,bwstd,bwmin,bwmax,self.bwtop,self.bwbot))
+            print('\n'.join(restab), '\n')
         bed.sort()
         beds = ['%s\t%d\t%d\t%s\t%d' % x for x in bed]
-        self.bedf.write('\n'.join(beds))
-        self.bedf.write('\n')
-        self.bedf.close()
-        print('\n'.join(restab), '\n')
+        bedf.write('\n'.join(beds))
+        bedf.write('\n')
+        bedf.close()
         print('Wrote %d bed regions' % len(bed))
     
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     a = parser.add_argument
-    a('--minwin',default=10, type=int)
-    a('--qfrac',default=0.01, type=float)
-    a('--bigwig',default="test.bw")
-    a('--bedout',default="test.bw.bed")
+    a('-m','--minwin',default=10, type=int)
+    a('-l', '--qlo',default=None, type=float)
+    a('-i', '--qhi',default=0.99, type=float)
+    a('-b', '--bigwig', nargs='+')
+    a('-n', '--bigwiglabels', nargs='+')
+    a('-o', '--bedout', required=True)
     args = parser.parse_args()
-    fo = findOut(bwname=args.bigwig, bedwin=args.minwin, qfrac=args.qfrac, bedout=args.bedout)
+    fo = findOut(bwname=args.bigwig, bwlabels=args.bigwiglabels, bedwin=args.minwin, qlo=args.qlo, qhi=args.qhi, bedout=args.bedout)
